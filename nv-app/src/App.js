095881a713fe5bb881d12f9bbdbf80729c299285
luis <6010ldaniel@gmail.com>
2022-11-15 17:28:29 -0300

    App.js

---
+++ nv-app/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <><h1 class='h'>Hola mundo</h1><br></br><p class="p">creo que al fin lo logre</p></>
+  )
+}
+
+export default App;
