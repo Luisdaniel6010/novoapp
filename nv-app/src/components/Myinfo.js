da00d57938a5f848644bdea513e549f45e437418
luis <6010ldaniel@gmail.com>
2022-11-16 20:49:39 -0300

    index.html

---
+++ nv-app/src/components/Myinfo.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function MyInfo() {
+  return(
+    <div>
+
+      <div>
+        <h1>Luis Bolivar</h1>
+        <p>Estas son algunas cosas sobre mi</p>
+        <ol>
+          <li>Estudiar de  mañana</li>
+          <li>Jugar Baloncesto</li>
+          <li>Compartir con mi familia</li>
+        </ol>
+      </div>
+
+    </div>
+  );
+}
+
+export default MyInfo;
