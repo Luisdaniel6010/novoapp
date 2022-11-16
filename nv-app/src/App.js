47d4db1dbf1774a9f57f4162b47affee3a6323b2
luis <6010ldaniel@gmail.com>
2022-11-15 21:31:06 -0300

    App.js

--- nv-app/src/App.js
+++ nv-app/src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useLayoutEffect } from 'react';
 
 function App() {
   return (
-   <><h1 class='h'>Hola mundo</h1><br></br><p class="p">creo que al fin lo logre</p></>
+   <><h1 class='h'>Hola mundo</h1><br></br><p class="p">Que haceres diarios</p>
+  
+   <ul>
+
+   <li>Estudiar</li>
+   <li>Trabajar</li>
+   <li>Compartir con la familia</li>
+
+ </ul>
+ </>
   );
 }
-
 export default App;
